6c48368570290709d200f07bd66cae925cdb7170
Dimiricus <getdimiricus@gmail.com>
2021-07-30 03:54:31 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,40 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  // если строка не чётная, значит не все скобки парные
+  if (str.length % 2 != 0) {
+    return false;
+  }
+
+  // скобочная пара, регулярка для поиска, определитель совпадения
+  let pair, regExp, haveFound; 
+
+  while(true) {
+
+    haveFound = false;
+
+    for (let i = 0; i < bracketsConfig.length; i++) {
+
+      if (str.length == 0) {
+        return true;
+      }
+
+      pair = bracketsConfig[i][0] + bracketsConfig[i][1];
+
+      if (str.indexOf(pair) != -1 ) {
+
+        //regExp = new RegExp(pair, 'g');
+        
+        str = str.replace(pair, '');
+
+        haveFound = true;
+      } 
+
+    }
+
+    if (haveFound == false) {
+      return false;
+    }
+
+  }
+
 }
